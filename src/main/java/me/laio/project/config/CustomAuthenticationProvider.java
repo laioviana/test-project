a0fa06549e990c5e8801a7e687d99c56861fe4b2
Laio Viana Bezerra Dias <laio.viana@SDA.CE.GOV.BR>
2018-07-31 14:29:18 -0300

    Project initialization

---
+++ src/main/java/me/laio/project/config/CustomAuthenticationProvider.java
@@ -0,0 +1,23 @@
+package me.laio.project.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String s, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
