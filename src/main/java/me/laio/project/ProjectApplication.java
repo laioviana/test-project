64354320ba1fefe501d48ef0e47b7db2b52dfbe0
Laio Viana Bezerra Dias <laio.viana@SDA.CE.GOV.BR>
2018-08-01 10:24:42 -0300

    Setting ProjectApplication to extends SpringBootInitializer

--- src/main/java/me/laio/project/ProjectApplication.java
+++ src/main/java/me/laio/project/ProjectApplication.java
@@ -2,11 +2,20 @@ package me.laio.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.util.Base64Utils;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+		//String teste = "Basic " + new String(Base64Utils.encode(("client:secret").getBytes()));
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectApplication.class);
 	}
 }
