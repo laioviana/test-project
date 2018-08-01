5344b28375c4049c29ab3f9b597f80f308218a56
Laio Viana Bezerra Dias <laio.viana@SDA.CE.GOV.BR>
2018-08-01 10:26:02 -0300

    Retrive user returning current user

--- src/main/java/me/laio/project/config/CustomAuthenticationProvider.java
+++ src/main/java/me/laio/project/config/CustomAuthenticationProvider.java
@@ -1,15 +1,23 @@
 package me.laio.project.config;
 
+import me.laio.project.domain.User;
+import me.laio.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
     private final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
@@ -17,7 +25,9 @@ public class CustomAuthenticationProvider extends AbstractUserDetailsAuthenticat
     }
 
     @Override
-    protected UserDetails retrieveUser(String s, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
-        return null;
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+        User user = userService.findUsuarioByLogin(username);
+
+        return user;
     }
 }
